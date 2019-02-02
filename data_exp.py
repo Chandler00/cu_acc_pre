30ba04af8faa02b0311365a51f5b6574ae6dd1e6
Chandler00 <28764722+Chandler00@users.noreply.github.com>
2019-02-01 22:31:10 -0500

    updated

--- data_exp.py
+++ data_exp.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan 28 21:17:19 2019
-
+project : customer product prediction
 @author: chandler qian
 
 """
 
 import pandas as pd
-
+from datetime import date, timedelta, datetime
 
 #%%
 #   1. data exploration
 # load data
 train_set = pd.read_csv('./train/train_monthly_info.csv')
 train_acc = pd.read_csv('./train/train_accounts.csv')
+train_acc['cust_key']=train_acc['cust_key'].astype(str)
+
 train_ref = pd.read_csv('./train/xref.csv')
 test_set =  pd.read_csv('./test/test_monthly_info.csv')
 
+
 #%%
-# used to develop template
-train_set = train_set[train_set['date']<='2015-08-28']
-train_acc = train_acc[train_acc['date']<='2015-08-28']
+# reduce sample size to develop template
+#train_set = train_set[train_set['date']<='2015-08-28']
+#train_acc = train_acc[train_acc['date']<='2015-08-28']
 
 # basic information
 train_set.head(5)
@@ -52,7 +55,7 @@ train_acc.info()
 # null value exploration, ' NA' not included
 acc_null_ratio = train_acc.isnull().sum(axis=0).sort_values(ascending=False)/len(train_acc)
 set_null_ratio = train_set.isnull().sum(axis=0).sort_values(ascending=False)/len(train_set)
-
+test_null_ratio = test_set.isnull().sum(axis=0).sort_values(ascending=False)/len(test_set)
 # 2. preprocessing
 # remove the column which contains very little information
 # 'deceased_status' , 'primary_address' - only 1 value, 'province_name' -duplicate with pro code, 
@@ -60,6 +63,10 @@ set_null_ratio = train_set.isnull().sum(axis=0).sort_values(ascending=False)/len
 train_set.drop(['primary_address', 'province_name', 'spouse_index'], axis =1, inplace=True)
 train_set = train_set[train_set['deceased_status']=='N']
 train_set.drop(['deceased_status'], axis =1, inplace=True)
+
+test_set.drop(['primary_address', 'province_name', 'spouse_index'], axis =1, inplace=True)
+test_set = test_set[test_set['deceased_status']=='N']
+test_set.drop(['deceased_status'], axis =1, inplace=True)
 
 # serious preprocessing for this task needed.
 # start from convert dataframe columns to proper datatype
@@ -90,9 +97,11 @@ for column in ['segment', 'channel', 'customer_relation', 'customer_type',
                 'province_code', 'sex']:
     
     re_common(train_set, column)
-
+    re_common(test_set, column)
+    
 # replace na from gross income to mean or median
 train_set['gross_income'].fillna(train_set['gross_income'].mean(skipna=True), inplace=True)
+test_set['gross_income'].fillna(test_set['gross_income'].mean(skipna=True), inplace=True)
 
 # if last_date_primary is confirm then return 1  if null then 0. 
 # after that i found it has same value of primary. so drop primary
@@ -108,6 +117,10 @@ def replace_value(content):
 train_set['last_date_primary'] = train_set['last_date_primary'].map(replace_value)       
 train_set['last_date_primary'].fillna(0, inplace=True)
 train_set.drop(['primary'], axis =1, inplace=True)
+
+test_set['last_date_primary'] = test_set['last_date_primary'].map(replace_value)       
+test_set['last_date_primary'].fillna(0, inplace=True)
+test_set.drop(['primary'], axis =1, inplace=True)
 
 #train_set['last_date_primary'].fillna(0, inplace=True)
 
@@ -126,6 +139,7 @@ for column in ['age', 'last_6_months_flag', 'seniority', 'primary',
     
     con_type(train_set, column, 'int')
 
+
 # replace and simplify values
 train_set['customer_type'] = train_set['customer_type'].astype('str')
 train_set['customer_type'].replace({'1.0':'pr', '1':'pr', '2':'co', '2.0':'co'
@@ -134,9 +148,27 @@ train_set['customer_type'].replace({'1.0':'pr', '1':'pr', '2':'co', '2.0':'co'
 train_set['domestic_index'].replace({'S':1, 'N':0}, inplace=True)
 train_set['foreigner_index'].replace({'S':1, 'N':0}, inplace=True)
 
+test_set['customer_type'] = test_set['customer_type'].astype('str')
+test_set['customer_type'].replace({'1.0':'pr', '1':'pr', '2':'co', '2.0':'co'
+         , '3':'fo_pr', '3.0':'fo_pr', '4.0':'fo_co', '4':'fo_co'}, inplace=True)
+    
+test_set['domestic_index'].replace({'S':1, 'N':0}, inplace=True)
+test_set['foreigner_index'].replace({'S':1, 'N':0}, inplace=True)
+
+
+# open date converted to date_diff(current date, opendate)
+
+def date_diff(tar_date):
+    
+    return (datetime.today() - datetime.strptime(tar_date, '%Y-%m-%d')).days
+#    return (date.today() - tar_date).days()
+
+train_set['open_days'] = train_set['open_date'].apply(date_diff)
+test_set['open_days'] = test_set['open_date'].apply(date_diff)
+
 # save output
 train_set.to_csv(r'.\preprocess\train_set.csv')
-
+test_set.to_csv(r'.\preprocess\test_set.csv')
 
 
 
--- feature_eng.py
+++ feature_eng.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 29 16:30:51 2019
-
+project : customer product prediction
 @author: chandler qian
 
 """
@@ -11,20 +11,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.feature_selection import VarianceThreshold
+import re
+
 #  free memory - garbage collector
 import gc
 gc.collect()
 
-#train_set = pd.read_csv(r'.\preprocess\train_set.csv')
-#train_set= train_set.iloc[:, 1:]
-#train_set.reset_index(inplace= True)
-#train_set['province_code'] = train_set['province_code'].astype('str')
+train_set = pd.read_csv(r'.\preprocess\train_set.csv', index_col=0)
+train_set.reset_index(inplace= True)
+train_set['province_code'] = train_set['province_code'].astype('str')
 
 #%%
 # encoding of the train_set
 # let's look at the existing columns and pick up the feature needs encoding.
 # columns ['date', 'cust_id', 'employee_index', 'country', 'sex', 'age',
-#       'open_date', 'last_6_months_flag', 'seniority', 
+#       'open_days', 'last_6_months_flag', 'seniority', 
 #       'last_date_primary', 'customer_type', 'customer_relation',
 #       'domestic_index', 'foreigner_index', 'channel', 'province_code',
 #       'activity_index', 'gross_income', 'segment']
@@ -32,14 +33,42 @@ gc.collect()
 # use one-hot encoding since not enough time for other encoding test
 # use one-hot to calculate variance first, 
 # label encoding is not working well with variance feature selection, so out of scope for now
-cat_fea_label = ['country', 'sex', 'customer_type', 'customer_relation',
-                 'channel', 'province_code','segment']
+cat_fea_label = ['employee_index','country', 'sex', 'customer_type', 'customer_relation',
+                 'domestic_index', 'foreigner_index', 'channel', 'province_code','segment']
 
-# set a copy of existing one for development purpose only
-test = train_set[cat_fea_label]
+non_cat_label = ['age', 'open_days', 'last_6_months_flag', 'seniority', 'last_date_primary',
+                 'activity_index','gross_income']
+
+pri_key = ['date', 'cust_id']
+
+# use minmax scalr at this time, want to make consisnt with other binary features.
+
+def nor_scaler(fe_list):
+    min_max_scaler = preprocessing.MinMaxScaler()
+
+    x_train_mima = min_max_scaler.fit_transform(fe_list)
+    
+    return x_train_mima
+
+# figure out seniority has anomaly, so most will be 0.99, remove outlier.
+# the income has high range, most income will be around 0 since max is extreme high, 
+# no time to change solution at this time.
+train_set.groupby(['seniority']).size()
+train_set['seniority'].replace({-999999:train_set['seniority'].value_counts().index[0]}
+                                , inplace = True)
+
+train_set[['age', 'open_days', 'seniority', 'gross_income']] = nor_scaler(
+        train_set[['age', 'open_days', 'seniority', 'gross_income']])   
+    
+test_set['seniority'].replace({-999999:test_set['seniority'].value_counts().index[0]}
+                                , inplace = True)
+
+test_set[['age', 'open_days', 'seniority', 'gross_income']] = nor_scaler(
+        test_set[['age', 'open_days', 'seniority', 'gross_income']])  
 
 # pandas's powerful onehot encoding method
-onehot = pd.get_dummies(test, prefix=None, prefix_sep= '_')
+onehot = pd.get_dummies( train_set[cat_fea_label], prefix=None, prefix_sep= '_')
+onehot_test = pd.get_dummies( test_set[cat_fea_label], prefix=None, prefix_sep= '_')
 
 # another way is to use label encoder which is tested however not prefered in this case
 #def label_en(df, column):
@@ -88,6 +117,8 @@ def var_selector(df, upper, lower, step):
         else:
             print ('no feature has higher variance than %s'%(threshold))
 
+# ideal call
+var_selector(onehot, 1, 0, 0.01)
 
 # loopthrough the variance and find the optimum point which can be expensive.
 # returns information : current testing threshold 0.200000000000000046
@@ -102,7 +133,14 @@ def var_selector(df, upper, lower, step):
 #['channel_KFA', 'channel_KFC', 'channel_KHE', 'channel_KHK','province_code_11.0', 'province_code_14.0', 'province_code_15.0',
 # 'province_code_18.0'] at NO.3 chunk 
 
-var_selector(onehot, 1, 0, 0.05)
+onehot_fea_final = ['sex_H', 'sex_V', 'customer_relation_A', 'customer_relation_I'
+                 ,'channel_KAT','channel_KFA', 'channel_KFC', 'channel_KHE'
+                 , 'channel_KHK','province_code_11.0', 'province_code_14.0'
+                 , 'province_code_15.0','province_code_18.0']
+
+# time to put trainset together
+x_train_final = train_set[(pri_key + non_cat_label)].join(onehot[onehot_fea_final]) 
+x_test_final = test_set[(pri_key + non_cat_label)].join(onehot_test[onehot_fea_final]) 
 
 # correlation coefficient check for features selected and manually picked features.
 def plot_cor(fe_df):
@@ -114,30 +152,85 @@ def plot_cor(fe_df):
     figure = corr_map.get_figure()
     figure.savefig('./vis/corr_conf.png')
 
-plot_cor(onehot[['sex_V', 'customer_relation_A']])
-#
-selector = VarianceThreshold(threshold=0.5)
-selector.fit_transform(test)
+# plot and save corr matrix, for sex_H and sex_V has -1 correlation for sure, no worry 
+# l2 regulization can handle this. for activity_index and cust relation has also high relation
+# which is good to know.
+plot_cor(x_train_final.iloc[:, 2:])
 
-train_set[cat_fea_label]
 
-from pandas import ExcelWriter
+x_train_final.to_csv(r'.\preprocess\x_train_final.csv')
+# 
 
-writer = ExcelWriter(r".\preprocess\test.xlsx")
+#%% relabel the train account, we need to find action of customers instead of existing accounts
+# we use the changes from previous to target month to build label, so intotal 15 months.
+# For example Jan 2015 we have customer  A B C D, Feb we have customer B C E. we only 
+# consider customer B C and their accounts as valid label for Feb data. 
+# this can be computational very expensive.
 
-test.iloc[0:300000, :].to_excel(writer)
+#train_acc.to_csv(r'.\preprocess\train_acc.csv')
+#train_acc = pd.read_csv(r'.\preprocess\train_acc.csv', index_col=0)
+#train_acc['cust_key'] = train_acc['cust_key'].astype('str')
 
-writer.save()
+delta = pd.merge(train_acc[train_acc['date'] == date_list[1]]
+            ,train_acc[train_acc['date'] == date_list[2]], on=['cust_key'], suffixes= ('/last', '/current'))
+
+
+# get a super large df first with dfs of both last month and current month
+def cust_monthly_label(df):
+    y_train = pd.DataFrame()
+    date_list = df.groupby('date').size().index.tolist()
+    
+    for i in range(0, len(date_list)-1):
+#                   len(date_list)):
+        in_join = pd.merge(df[df['date'] == date_list[i]]
+            ,df[df['date'] == date_list[i+1]], on=['cust_key'], suffixes= ('/last', '/current'))
+        y_train = y_train.append(in_join)
+    
+    return y_train
+
+y_train = cust_monthly_label(train_acc)
+
+
+# get the delta value for each account with cust_key and current date
+def delta_table(df):
+    y_train_final = pd.DataFrame()
+    y_train_final['date'] = df['date/current']
+    y_train_final['cust_key'] = df['cust_key']
+    al_col=df.columns.tolist()
+    regex_la = re.compile(".*/last")
+    regex_cu = re.compile(".*/current")
+#   exclude the date_last which is not needed for delta
+    last_cols = list(filter(regex_la.match, al_col))[1:]
+    print (last_cols)
+#    cu_cols = list(filter(regex_cu.match, al_col))
+    
+    for col in last_cols:
+        delta_name = col.split('/')[0]
+        y_train_final[delta_name] = df[delta_name+'/current'] - df[col]
+    
+    return y_train_final
+
+y_train_final = delta_table(y_train)
+
+def fre_des(df):
+    
+    for column in df.columns:
+        print (df[column].value_counts(dropna=False).nlargest(20))
+
+fre_des(y_train_final)
+# -1 value found which is totally right, but we don't need to investigate at this point
+
+y_train_final.replace({-1:0}, inplace=True)
+
+y_train_final.to_csv(r'.\preprocess\y_train_final.csv')
+
+
+
+# validation step
+#jan = train_acc[train_acc['date'] == '2015-01-28']['cust_key']
+#feb = train_acc[train_acc['date'] == '2015-02-28']['cust_key']
+#jan.to_csv(r'.\preprocess\jan_acc.csv')
+#feb.to_csv(r'.\preprocess\feb_acc.csv')
 
 #%%
 
-
-jan = train_set[train_set['date'] == '2015-01-28']
-
-feb = train_set[train_set['date'] == '2015-02-28']
-
-inv = [i for i in jan['cust_id'] if i not in feb['cust_id']]
-
-
-# we are developping a model to see the delta of products by month. 
-# the current existing products of customers can not be used as label.
